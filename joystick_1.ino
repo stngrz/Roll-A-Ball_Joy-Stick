3bbc116d918f801ba319d66d049cf3214d6503d2
Jacob Kolb <jkolb12@gmail.com>
2017-07-13 17:34:23 -0600

    Add files via upload
    Code offers some initialization to reduce drift when at neutral.

---
+++ joystick_1.ino
@@ -0,0 +1,42 @@
+int valX = 0;
+int valY = 0;
+int initializeX;
+int initializeY;
+const int joyPinX = A0;
+const int joyPinY = A1;
+
+void setup() {
+  pinMode(joyPinX, INPUT);              
+  pinMode(joyPinY, INPUT); 
+  Serial.begin(9600);
+  initializeX = map(analogRead(joyPinX),0,1023,-100,100)*-1;
+  initializeY = map(analogRead(joyPinY),0,1023,-100,100)*-1;
+
+}
+
+void loop() {
+  valX = map(analogRead(joyPinX),0,1023,-100,100)+initializeX;
+  valY = map(analogRead(joyPinY),0,1023,-100,100)+initializeY;
+  if(valX>=2 ){
+    valX=1;
+  }
+  else if(valX<=-2){
+    valX=-1;
+  }
+  else{
+    valX=0;
+  }
+  if(valY>=2){
+    valY=1;
+  }
+  else if(valY<=-2){
+    valY=-1;
+  }
+  else{
+    valY=0;
+  }
+  Serial.print(valX);
+  Serial.print(",");
+  Serial.println(valY);
+  delay(20);
+}
